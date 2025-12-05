c69f300310ec92cb12b16af34e578895bae68670
서석규 <ssk7594@gmail.com>
2025-12-05 02:53:04 +0000

    Feat: 초기 백엔드 구조 및 Prisma 설정 추가 (Render ENOENT 해결을 위한 package.json 포함)

---
+++ prisma/seed.js
@@ -0,0 +1,41 @@
+// prisma/seed.js
+import { prisma } from "./prisma.js";
+
+async function main() {
+  // 기존 데이터 삭제 (초기화)
+  console.log("--- 시딩 시작 ---");
+  console.log("Delete previous data");
+  await prisma.article.deleteMany();
+
+  // 더미 데이터 생성
+  console.log("Seed dummies");
+  const payload = await prisma.article.createMany({
+    data: [
+      {
+        title: "title1",
+        content: "content1",
+      },
+      {
+        title: "title2",
+        content: "content2",
+      },
+      {
+        title: "title3",
+        content: "content3",
+      },
+    ],
+  });
+
+  console.log(payload.count, " dummies seeded");
+  console.log("--- 시딩 끝 ---");
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
